19e3b8b1be002af80964eab63c600b1eeeb2a2be
khayrulislam <bsse0822@iit.du.ac.bd>
2019-09-04 00:12:23 +0600

    create a new project

---
+++ DecisionTree/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DecisionTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
