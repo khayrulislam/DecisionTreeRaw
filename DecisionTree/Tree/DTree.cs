f3546da33c94efbccf3a12bfc65c17516cb302f6
khayrulislam <bsse0822@iit.du.ac.bd>
2019-09-04 12:52:16 +0600

    DTree class create for printing the decission tree. printing method implementation not done

---
+++ DecisionTree/Tree/DTree.cs
@@ -0,0 +1,44 @@
+﻿using DecisionTree.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecisionTree.Tree
+{
+    public sealed class DTree
+    {
+
+        public static DTree treeInstance = null;
+
+        public DTreeNode root { get; private set; }
+
+        public void Storeroot(DTreeNode node)
+        {
+            this.root = node;
+        }
+
+        private DTree()
+        {
+        }
+
+        public static DTree GetDTreeInstance
+        {
+            get
+            {
+                if(treeInstance == null)
+                {
+                    treeInstance = new DTree();
+                }
+                return treeInstance;
+            }
+        }
+
+        // print tree
+        public void PrintDecisionTree()
+        {
+
+        }
+
+
+    }
+}
