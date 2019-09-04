899b94ec0f00f4ef435373ac51a969581860cadf
khayrulislam <bsse0822@iit.du.ac.bd>
2019-09-04 12:18:54 +0600

    create a tree node for decision tree

---
+++ DecisionTree/Data/DTreeNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecisionTree.Data
+{
+    public class DTreeNode
+    {
+        public string spliteFeatureName { get; set; }
+        public string spliteFeatureValue { get; set; }
+        public double entropy { get; set; }
+        public double informationGain { get; set; }
+        public List<DTreeNode> childrenNodes { get; set; }
+        public string className{ get; set; }
+        public bool isLeaf { get; set; }
+
+        public DTreeNode()
+        {
+            this.isLeaf = false;
+        }
+    }
+}
