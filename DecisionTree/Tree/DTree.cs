8312c191ef3f7a89dadb41b18ac2074e9a27ee61
khayrulislam <bsse0822@iit.du.ac.bd>
2019-09-09 19:21:47 +0600

    tree stucture is print in the console

--- DecisionTree/Tree/DTree.cs
+++ DecisionTree/Tree/DTree.cs
@@ -1,6 +1,7 @@
 ﻿using DecisionTree.Data;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DecisionTree.Tree
@@ -41,16 +42,19 @@ namespace DecisionTree.Tree
 
         private void PrintTree(DTreeNode currentNode, string indent, bool last)
         {
-            string value = currentNode.spliteFeatureValue != "" ? "(" + currentNode.spliteFeatureValue + ") " : "" ;
-            string ans = currentNode.className !="" ? " " +currentNode.className : "" ;
+            string data = currentNode.spliteFeatureValue != null ? "--"+currentNode.spliteFeatureValue : "";
+            string feature = currentNode.spliteFeatureName !=null ? "---[" + currentNode.spliteFeatureName + "]" : "";
+            string ans = currentNode.className != null ? "---("+currentNode.className+")" : "";
 
-            Console.WriteLine(indent + "+--"+ value + currentNode.spliteFeatureName + ans + currentNode.isLeaf);
-            indent += last ? "   " : "|  ";
+            Console.WriteLine(indent + "+"+ data + feature + ans);
+            indent += last ? " " : "|";
+            for (int i = 0; i < data.Length+3; i++) indent += " ";
 
             for (int i = 0; i < currentNode.childrenNodes.Count; i++)
             {
                 PrintTree(currentNode.childrenNodes[i], indent, i == currentNode.childrenNodes.Count - 1);
             }
         }
+
     }
 }
